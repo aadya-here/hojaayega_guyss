0f3ac886bd6314dc60a5574b1147d70034bd8c0a
kunal <kishujaiswal3@gmail.com>
2024-07-06 21:10:34 +0530

    Added Profile Page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignInPage from './pages/Signin';
-import SignUpPage from './pages/Signup';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import { VendorProvider } from './context/vendorContext';
-import VendorLoginPage from './pages/SigninV';
-import { Test } from './pages/Test';
-import Forms from './pages/Forms';
-import BottomNavbar from './components/BottomNavbar';
-import FormsList from './pages/FormsList';
-import FormViewById from './pages/FormViewById';
-import ProjectView from './pages/ProjectView';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/Signin";
+import SignUpPage from "./pages/Signup";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import { VendorProvider } from "./context/vendorContext";
+import VendorLoginPage from "./pages/SigninV";
+import { Test } from "./pages/Test";
+import Forms from "./pages/Forms";
+import BottomNavbar from "./components/BottomNavbar";
+import FormsList from "./pages/FormsList";
+import FormViewById from "./pages/FormViewById";
+import ProjectView from "./pages/ProjectView";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -22,18 +22,20 @@ const App = () => {
           <VendorProvider initialVendorId={null}>
             <Routes>
               <Route path="/signin" element={<SignInPage />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/projects" element={<Projects />} />
               <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/projects" element={<Projects />} />
               <Route path="/vendor-login" element={<VendorLoginPage />} />
               <Route path="/test" element={<Test />} />
 
-
               <Route path="/forms" element={<Forms />} />
               <Route path="/forms-view" element={<FormsList />} />
-              <Route path="/forms-view/:formName/:formId/:formLogId" element={<FormViewById />} />
+              <Route
+                path="/forms-view/:formName/:formId/:formLogId"
+                element={<FormViewById />}
+              />
               <Route path="/projects/:projectId" element={<ProjectView />} />
-
             </Routes>
           </VendorProvider>
         </div>
